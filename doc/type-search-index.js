426bbc045f4929731396434730939ff1759fdf74
gilangyudharaka <gilang.yudharaka@ui.ac.id>
2019-04-18 14:46:45 +0700

    PreCS 6

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"DatabaseItem"},{"p":"<Unnamed>","l":"DatabaseSupplier"},{"p":"<Unnamed>","l":"Invoice"},{"p":"<Unnamed>","l":"Item"},{"p":"<Unnamed>","l":"JStore"},{"p":"<Unnamed>","l":"Location"},{"p":"<Unnamed>","l":"Supplier"}]
